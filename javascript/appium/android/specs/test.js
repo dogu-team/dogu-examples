9d35f342edda66a503aaf9d9b866cc6f3bafc1e9
Byoungwook Park <qkrquddnr99@gmail.com>
2023-07-07 20:25:54 +0900

    wip: javascript webdriverio android example

--- javascript/appium/android/specs/test.js
+++ javascript/appium/android/specs/test.js
@@ -1,18 +1,35 @@
-var assert = require('assert');
+import assert from 'assert';
+import { remote } from 'webdriverio'
 
-describe('Search Wikipedia Functionality', () => {
-  it('can find search results', async () => {
-    var searchSelector = await $(`~Search Wikipedia`);
-    await searchSelector.waitForDisplayed({ timeout: 30000 });
-    await searchSelector.click();
 
-    var insertTextSelector = await $('android=new UiSelector().resourceId("org.wikipedia.alpha:id/search_src_text")');
-    await insertTextSelector.waitForDisplayed({ timeout: 30000 });
+const browser = await remote({
+  hostname: 'api.dogutech.io',
+  port: 80,
+  path: '/wd/hub',
+  capabilities: {
+    platformName: "android",
+    'appium:automationName': "uiautomator2",
+    "dogu:options": {
+        accessKey: "",
+        organizationId: "",
+        projectId: "",
+        tag: "android",
+        appVersion: "2.5.194-alpha-2017-05-30",
+    },
+  }
+})
 
-    await insertTextSelector.addValue("Wikipedia");
-    await browser.pause(5000);
+var searchSelector = await browser.$(`~Search Wikipedia`);
+await searchSelector.waitForDisplayed({ timeout: 30000 });
+await searchSelector.click();
 
-    var allProductsName = await $$(`android.widget.TextView`);
-    assert(allProductsName.length > 0);
-  });
-});
+var insertTextSelector = await browser.$('android=new UiSelector().resourceId("org.wikipedia.alpha:id/search_src_text")');
+await insertTextSelector.waitForDisplayed({ timeout: 30000 });
+
+await insertTextSelector.addValue("Wikipedia");
+await browser.pause(5000);
+
+var allProductsName = await browser.$$(`android.widget.TextView`);
+assert(allProductsName.length > 0);
+
+await browser.deleteSession();
