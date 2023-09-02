00c83c46f791e7ef13b22f228d1d7b1fdcdb261c
oneofthezombies <hunhoekim@gmail.com>
2023-09-02 15:53:50 +0900

    chore: change timeout

--- webdriverio/javascript/jest/web/web.test.js
+++ webdriverio/javascript/jest/web/web.test.js
@@ -1,6 +1,6 @@
 test("go to dogutech.io", async () => {
   await driver.url("https://dogutech.io");
-});
+}, 10_000);
 
 test("search for dogu", async () => {
   this.doguElements = await driver.$$('xpath://*[contains(text(), "dogu")]');
